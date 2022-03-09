439ed9ff7e24c620f9071ee7c7c1e9a3a85709ca
Jacksori <soljae95@gmail.com>
2022-03-10 00:16:39 +0900

    1.1v, git 프로젝트 이사

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import AppRouter from './Router'
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn}  />
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+  );
+  // return <AppRouter />;
+}
+
+export default App;
